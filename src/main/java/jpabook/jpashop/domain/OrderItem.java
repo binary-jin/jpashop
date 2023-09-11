59988e7fdceaaea0ba55ed4142fe9e6c264ab34e
yi-jin <dlwls4338@naver.com>
2023-09-11 23:42:20 +0900

    주문 관리 기능 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "order_item")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //new로 새로 생성하지 못하게 막음
 public class OrderItem {
 
     @Id
@@ -28,4 +31,23 @@ public class OrderItem {
 
     private int count; //주문 수량
 
+    //==생성 메서드==
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==
+    public void cancel() {
+        getItem().addStock(count); //재고 수량의 원상복귀
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
