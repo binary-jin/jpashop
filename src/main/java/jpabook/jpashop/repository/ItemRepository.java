3afb3ec44be0fea40d443bd311f7bc1fd183790a
yi-jin <dlwls4338@naver.com>
2023-09-11 17:27:19 +0900

    상품 관리 기능 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item); //맨 처음에 상품이 없으면 id가 null임 그럴 땐 persist를 사용해 저장
+        } else {
+            em.merge(item); //업데이트랑 비슷한거
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
